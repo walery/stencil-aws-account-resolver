74c710deec3031e2565b143b727b0001aea7c561
Walery Strauch <git-commits-a3dm@walery.org>
2022-05-25 23:46:47 +0200

    Replace old name by new one [ib2 -> stencil].

--- blocks/companyTld.js
+++ blocks/companyTld.js
@@ -1,3 +1,3 @@
 module.exports.resolve = ({variableUtils}) => {
-  return variableUtils.resolveVariable('ssm(us-east-1):/ib2/aws/companyTld');  // TODO ib2 ref
+  return variableUtils.resolveVariable('ssm(us-east-1):/stencil/aws/companyTld');
 };
